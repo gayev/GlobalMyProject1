193da5fa0f760cf0fc29c3b1230173c843f0515d
gayev <danik120697@mail.ru>
2017-11-23 15:39:28 +0300

    add function MYpoW() version 2

--- guncs.cpp
+++ guncs.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "funcs.h"  
+#include <cmath>
  
 double MySum(double a, double b)
 {  return a + b; } 
@@ -12,3 +13,7 @@ double MyMul(double a, double b)
 
 double MyDiv(double a, double b)
 {  return a / b; }
+double MyPow(double a, double b)
+{
+	return exp(b * log(a));
+}
